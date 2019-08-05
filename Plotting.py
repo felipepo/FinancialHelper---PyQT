771703b2c6c7b9249ba147b80fdea04a63a81408
Felipe Gomes Pontes <felipegomespontes@hotmail.com>
2019-08-05 17:38:23 -0300

    ...

--- Plotting.py
+++ Plotting.py
@@ -3,7 +3,6 @@ import matplotlib
 matplotlib.use('Qt5Agg')
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
-import matplotlib.pyplot as plt
 import numpy as np
 import SQLDB
 import Funs
